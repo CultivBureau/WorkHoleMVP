154a860fc5338e44d11eebd13097686ab6f0aad5
Abdallah Wael <bedowael365@gmail.com>
2025-08-12 13:26:45 +0300

    solve the aside problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import './App.css'
-import SideMenu from './components/side-menu/side-menu'
-import NavBar from './components/NavBar/navbar'
+import "./App.css";
+import SideMenu from "./components/side-menu/side-menu";
+import NavBar from "./components/NavBar/navbar";
+
 function App() {
   return (
-    <div className='w-full h-screen ' style={{ background: 'var(--bg-color)' }}>
-      <SideMenu />
-      <NavBar/> 
+    <div
+      className="w-full h-screen flex flex-col"
+      style={{ background: "var(--bg-color)" }}
+    >
+      {/* Navigation Bar - Full Width at Top */}
+      <NavBar />
+
+      {/* Content Area with SideMenu and Main Content */}
+      <div className="flex flex-1 min-h-0">
+        {/* Side Menu - Left side under navbar */}
+        <SideMenu />
+
+        {/* Main Content - Rest of the space */}
+        <main className="flex-1 overflow-auto p-4">
+          <div
+            className="h-full rounded-2xl border border-gray-200"
+            style={{ background: "var(--bg-color)" }}
+          >
+            {/* Dashboard content */}
+          </div>
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
