2754dd169224213bfa9d932f20b5ea9675c3ca99
amnayahia <100522656+26amna@users.noreply.github.com>
2025-08-12 12:51:57 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
 import SideMenu from './components/side-menu/side-menu'
-
 function App() {
   return (
-    <div className='w-full h-screen flex' style={{ background: 'var(--bg-color)' }}>
+    <div className='w-full h-screen ' style={{ background: 'var(--bg-color)' }}>
       <SideMenu />
       <main className='flex-1 p-6'>
         {/* Page content placeholder */}
