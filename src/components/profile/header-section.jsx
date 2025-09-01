7f382fd06dfcb5166e347f8c494e9ee95addc928
amnayahia <100522656+26amna@users.noreply.github.com>
2025-09-01 15:41:48 +0300

    ..

--- src/components/profile/header-section.jsx
+++ src/components/profile/header-section.jsx
@@ -6,6 +6,8 @@ import { useTranslation } from "react-i18next"
 const HeaderSection = ({ userData }) => {
   const { t } = useTranslation()
    
+
+  
   return (
     <div>
       <div className="flex items-start justify-between mb-8">
